9cc70c962614cc5434f2704a781eebf6ef293e2f
Sahdev <kumarsahdev315@gmail.com>
2026-01-21 21:59:16 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose"
+import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 
@@ -14,5 +14,12 @@ app.use(express.json({ limit : "16kb"}))
 app.use(express.urlencoded({ extended : true, limit : "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+
+// import routes
+import userRouter from "./routes/user.routes.js";
+
+// routes declaration
+app.use("/api/v1/users", userRouter)
 
 export { app }
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+// Configuration
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: CLOUDNIARY_API_KEY, 
+    api_secret: CLOUDINARY_API_SECRET 
+});
+
+// upload an image
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) return null
+        // upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type : 'auto'
+        })
+        // file has been uploaded successfull
+        console.log("file is uploaded on cloudinary", response.url);
+        return response
+    } catch (error) {
+        // remove the locally saved temporary file as the upload operation got failed
+        fs.unlinkSync(localFilePath) 
+        return null;
+    }
+}
+
+export {uploadOnCloudinary}
